579fb69ff3a05e2598a50b49146bac852ae2fb14
QueebSkeleton <queebskeleton@gmail.com>
2021-02-09 01:39:15 +0800

    [Refactor][Document] Match domain requirements for creating Activities

--- src/main/java/com/github/queebskeleton/schlmgmt/domain/Activity.java
+++ src/main/java/com/github/queebskeleton/schlmgmt/domain/Activity.java
@@ -2,22 +2,50 @@ package com.github.queebskeleton.schlmgmt.domain;
 
 import java.time.LocalDateTime;
 
+/**
+ * Domain class for a Subject activity.
+ * 
+ * @author Rian Carlo Reyes
+ *
+ */
 public class Activity {
 	
+	// Database Id
 	private int id;
+	// Activity Name
 	private String name;
+	// When this Activity was created
 	private LocalDateTime createdOn;
+	// Deadline of this Activity
 	private LocalDateTime deadline;
+	// Total Score of this Activity. For grading purposes
 	private int totalScore;
 	
-	public Activity(String name, LocalDateTime createdOn, LocalDateTime deadline, int totalScore) {
+	/**
+	 * Creates a fresh activity with the specified properties.
+	 * 
+	 * @param name the name of the activity
+	 * @param deadline deadline to be set
+	 * @param totalScore highest achievable score of this activity
+	 */
+	public Activity(String name, LocalDateTime deadline, int totalScore) {
 		super();
 		this.name = name;
-		this.createdOn = createdOn;
+		this.createdOn = LocalDateTime.now();
 		this.deadline = deadline;
 		this.totalScore = totalScore;
 	}
 
+	/**
+	 * Constructs a new Activity with pre-initialized data.
+	 * Used by JDBC when fetching already persisted Activities.
+	 * 
+	 * @param id the database id
+	 * @param name the name of the activity
+	 * @param createdOn when this activity was created
+	 * @param deadline deadline to be set
+	 * @param totalScore highest achievable score of this activity
+	 */
 	public Activity(int id, String name, LocalDateTime createdOn, LocalDateTime deadline, int totalScore) {
 		super();
 		this.id = id;
